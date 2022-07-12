7c84366f0785b8e59605c3ab05261d9c45536e7f
kmynguyen24hdev <mynguyen.24hdev@gmail.com>
2022-07-12 10:17:29 +0700

    UI header 1

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,14 +1,40 @@
-import classNames from 'classnames/bind';
-import styles from './Header.module.scss';
+import classNames from "classnames/bind";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCircleXmark,
+  faMagnifyingGlass,
+  faSpinner,
+} from "@fortawesome/free-solid-svg-icons";
 
-const cx = classNames.bind(styles)
+import styles from "./Header.module.scss";
+import images from "~/assets/images";
+
+// provides a simple function for gluing classnames together
+const cx = classNames.bind(styles);
 
 function Header() {
-    return <header className={cx('wrapper')}>
-        <div className={cx('inner')} >
-            
+  return (
+    <header className={cx("wrapper")}>
+      <div className={cx("inner")}>
+        <div className={cx("logo")}>
+          <img src={images.logo} alt="logo" />
         </div>
-    </header>;
+        <div className={cx("search")}>
+          <input placeholder="Search accounts and videos" spellCheck={false} />
+          <button className={cx("clear")}>
+            {<FontAwesomeIcon icon={faCircleXmark} />}
+          </button>
+          <FontAwesomeIcon icon={faSpinner} className={cx("loading")} />
+          <button className={cx("search-btn")}>
+            {<FontAwesomeIcon icon={faMagnifyingGlass} />}
+          </button>
+        </div>
+        <div className={cx("action")}>
+
+        </div>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
